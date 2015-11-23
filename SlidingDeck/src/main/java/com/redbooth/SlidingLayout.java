fa267e4553dbb105233df9271c433c6332cf2c3c
Txus Ballesteros <txus.ballesteros@gmail.com>
2015-11-23 15:12:13 +0100

    Adding Behaviours to Items

--- SlidingDeck/src/main/java/com/redbooth/SlidingLayout.java
+++ SlidingDeck/src/main/java/com/redbooth/SlidingLayout.java
@@ -3,11 +3,14 @@ package com.redbooth;
 import android.content.Context;
 import android.support.annotation.NonNull;
 import android.util.TypedValue;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.FrameLayout;
 
 class SlidingLayout extends FrameLayout {
+    private float initialTouchPositionX;
+    private int currentOffsetX;
     public static final int WITHOUT_MARGIN = 0;
     public static final int MINIMUM_OFFSET_TOP_BOTTOM_IN_DP = 10;
     public static final int MINIMUM_OFFSET_LEFT_RIGHT_IN_DP = 10;
@@ -44,6 +47,31 @@ class SlidingLayout extends FrameLayout {
         applyOffsets();
     }
 
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        if (zIndex != 0) {
+            return super.onTouchEvent(event);
+        }
+        boolean result = true;
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                currentOffsetX = 0;
+                initialTouchPositionX = event.getX();
+                break;
+            case MotionEvent.ACTION_MOVE:
+                int offset = (int)(event.getX() - initialTouchPositionX);
+                currentOffsetX += offset;
+                offsetLeftAndRight(offset);
+                break;
+            case MotionEvent.ACTION_UP:
+                offsetLeftAndRight(-currentOffsetX);
+                break;
+            default:
+                result = super.onTouchEvent(event);
+        }
+        return result;
+    }
+
     private void applyOffsets() {
         if (zIndex > 0) {
             int topBottomOffset = calculateTopAndBottomOffset();
@@ -57,7 +85,7 @@ class SlidingLayout extends FrameLayout {
                                                         getResources().getDisplayMetrics());
         float offset = minimumOffset * zIndex;
         if (zIndex > 1) {
-            offset = offset - (minimumOffset * (zIndex / 10f));
+            offset = offset - (minimumOffset * ((float)zIndex / 10f));
         }
         return (int)offset;
     }
