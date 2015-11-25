4a7f9de9d309459ee7165f0c1a7d6a8bc8320e8d
Txus Ballesteros <txus.ballesteros@gmail.com>
2015-11-25 14:25:00 +0100

    Adding interactive view to childs

--- app/src/main/java/com/redbooth/demo/SlidingDeckAdapter.java
+++ app/src/main/java/com/redbooth/demo/SlidingDeckAdapter.java
@@ -6,6 +6,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class SlidingDeckAdapter extends ArrayAdapter<SlidingDeckModel> {
     public SlidingDeckAdapter(Context context) {
@@ -21,6 +22,12 @@ public class SlidingDeckAdapter extends ArrayAdapter<SlidingDeckModel> {
         }
         SlidingDeckModel item = getItem(position);
         ((TextView)view.findViewById(R.id.elementTitle)).setText(item.getTitle());
+        view.findViewById(R.id.command).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(v.getContext(), "Command", Toast.LENGTH_SHORT).show();
+            }
+        });
         return view;
     }
 }
